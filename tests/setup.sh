73f4faff0c656a0800c27be896b347ba6589eaea
Von Welch <von@vwelch.com>
2014-04-19 08:54:51 -0400

    Added testing of password store initialization.

--- tests/setup.sh
+++ tests/setup.sh
@@ -12,4 +12,28 @@ else
 	exit 1
 fi
 
+#
+# GNNUPG configuration
+
+# Where the test keyring and test key id
+# Note: the assumption is the test key is unencrypted.
+export GNUPGHOME=$(pwd)"/gnupg/"
+export PASSWORD_STORE_KEY=3DEEA12D  # "Password-store Test Key"
+
+# Initialize a password store, setting PASSWORD_STORE_DIR
+pass_init() {
+	export PASSWORD_STORE_DIR=${SHARNESS_TRASH_DIRECTORY}/test-store/
+	echo "Initializing test password store (${PASSWORD_STORE_DIR}) with key ${PASSWORD_STORE_KEY}"
+
+	# This curently returns non-zero for unknown reasons.
+	# Only happens with stdin set to /dev/null.
+	# I suspect the agent check.
+	# TODO:  Once fixed, catch non-zero here and fail.
+	${PASS} init ${PASSWORD_STORE_KEY} || true
+
+	echo "Initilization of ${PASSWORD_STORE_DIR} complete."
+}
+
+
+# Initialize the test harness
 . ./sharness.sh
--- tests/t0001-sanity-checks.sh
+++ tests/t0001-sanity-checks.sh
@@ -6,4 +6,11 @@ test_description='Sanity checks'
 test_expect_success 'Make sure we can run pass' '
 	${PASS} --help | grep "pass: the standard unix password manager"
 '
+
+test_expect_success 'Make sure we can initialize our test store' '
+	pass_init &&
+	ls -ld "${PASSWORD_STORE_DIR}" &&
+	[[ -d "${PASSWORD_STORE_DIR}" ]]
+'
+
 test_done
