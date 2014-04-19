759d0a6dc3fc930dcc9255e80654731abfe8f220
Von Welch <von@vwelch.com>
2014-04-19 15:25:41 -0400

    t0100-insert-tests.sh: 'insert -e' now working. Not sure what fixed.

--- tests/t0100-insert-tests.sh
+++ tests/t0100-insert-tests.sh
@@ -6,8 +6,7 @@ test_description='Test insert'
 export TEST_CRED="test_cred"
 export TEST_PASSWORD="Hello world"
 
-# Exposes some bug with 'insert -e' returning non-zero despite working.
-test_expect_failure 'Test "insert" command' '
+test_expect_success 'Test "insert" command' '
 	pass_init &&
 	echo "$TEST_PASSWORD" | ${PASS} insert -e "$TEST_CRED" &&
 	verify_password "$TEST_CRED" "$TEST_PASSWORD"
